0cd8e4cb0d3f6edc6ca54caacb209f0a41147874
Neha Nihar <niharneha.1997@gmail.com>
2024-09-04 12:25:28 +0530

    updated data.sql query

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO user (user_id,first_name, user_name) VALUES
-  (1,'Jigar', 'jj'),
-  (2,'Nilisha', 'ns');
+INSERT INTO user (user_id,first_name, last_name, user_name, password, date_of_birth) VALUES
+  (1,'Jigar', 'Patel', 'jj', 'password1', '1990-01-01T00:00:00'),
+  (2,'Nilisha', 'Shah', 'ns', 'password2', '1992-02-02T00:00:00');
